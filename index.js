f3c65f568a284f336c36c72b764e88115afe641d
ikhushisingla <singlakhushi822@gmail.com>
2023-08-19 02:14:47 +0530

    sungle product page done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Home page
+
 const bar= document.getElementById('bar');
 const nav=document.getElementById('navbar');
 const close=document.getElementById('close')
@@ -12,4 +14,23 @@ if(close){
     close.addEventListener('click' ,()=>{
         nav.classList.remove('active')
     })
+}
+
+
+// single products details
+
+var MainImg=document.getElementById("MainImg");
+var smallimg=document.getElementsByClassName("small-img")
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
 }
